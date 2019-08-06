a323c0e6862164b9b89886795d6d2c203f04c7a4
Madalina Jantea <madalina.jantea@gmail.com>
2019-08-06 13:57:44 -0700

    modif. index.js

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 const fs = require('fs');
-fs.writeFile('hello-world.txt', 'Hello to this great world', 'utf8', (err) => {
+const http = require('http');
+const port = 8000;
+const hello =require('./hello-world.js');  
+const helloWorld=hello();
+console.log(helloWorld);
+
+const requestHandler = (request, response) => {
+  fs.writeFile('hello-world.txt', 'Hello to this great world', 'utf8', (err) => {
     if (err) throw err;
     console.log('Success')
   });
-
-
-  const http = require('http');
-  const port = 8000;
-  
-  const requestHandler = (request, response) => {
     response.end(`Handling a request on port ${port}`)
   };
   
-  const server = http.createServer(requestHandler);
+const server = http.createServer(requestHandler);
   
-  server.listen(port, (err) => {
+server.listen(port, (err) => {
     if (err) {
       return console.log(`You have an error:  ${err}`);
     }
   
     console.log(`Server is listening on ${port}`);
+
   });
 
-const hello =require('./hello-world.js');
-const helloWorld=hello();
-console.log(helloWorld);
+
 
